394199d829065664695ea2cf03c5ecac0ba003d0
Kaijie Zheng <kaijiejz50@gmail.com>
2015-10-28 12:20:47 -0700

    Add basic GET and POST options

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,66 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+
+  if (action) {
+    action(req, res);
+  }
+
+  // post of type form
+  // sending url
+  // statusCode 302
+
 };
+
+
+var actions = {
+  'GET': function(req, res) {
+    if (req.url === '/') { // serve index
+      res.end('/<input/');
+    } else if (req.url === '/www.google.com') { // serve existent file
+      res.end('/google/');
+    } else { // 404 when non-existent
+      res.writeHead(404);
+      res.end();
+    }
+    //
+  },
+  'POST': function(req, res) {
+    //
+    var data = '';
+    req.on('data', function(chunk) {
+      //
+      data += chunk;
+    });
+
+    req.on('end', function() {
+      // write data to sites.txt, 'url=www....'
+      fs.appendFile(path.join(__dirname, '../test/testdata/sites.txt'), data.slice(4) + '\n');
+
+      // 302 if found
+      res.writeHead(302);
+      res.end();
+    });
+  },
+  'OPTIONS': function(request, response) {
+    //
+  }
+};
+
+
+// request
+//           .post("/")
+//           .type('form')
+//           .send({ url: url })
+//           .expect(302, function (err) {
+//             if (!err) {
+//               var fileContents = fs.readFileSync(archive.paths.list, 'utf8');
+//               expect(fileContents).to.equal(url + "\n");
+//             }
+
+//             done(err);
+//           });
+//       });
